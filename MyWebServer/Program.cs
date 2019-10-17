2dd0a0062b1836d922510cd4dc8ef032c3107377
eraiser <krotttendorfer@hotmail.de>
2019-10-17 16:44:35 +0200

    das ist der webserver aus der Vorlage du undankbarer Hundesohn

--- MyWebServer/Program.cs
+++ MyWebServer/Program.cs
@@ -3,84 +3,74 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 
 using MyWebServer;
 
-namespace Main { 
+namespace Main
+{
     class Program
     {
         static void Main(string[] args)
         {
-
-            var s = GetValidRequestStream("/");
-            var z = new MemoryStream();
-            s.CopyTo(z);
-            s.Seek(0, SeekOrigin.Begin);
-            z.Seek(0, SeekOrigin.Begin);
-
-            var rw = new StreamReader(s);
-
-            string line;
-
-            while ( (line = rw.ReadLine()) != null)
-            {
-                Console.WriteLine(line);
-            }
-
-            s.Seek(0, SeekOrigin.Begin);
-            rw = new StreamReader(z);
-            while ((line = rw.ReadLine()) != null)
-            {
-                Console.WriteLine(line);
-            }
-
-
-            Console.ReadLine();
-
+            Read();
+            Listen();
         }
-        public static Stream GetValidRequestStream(string url, string method = "GET", string host = "localhost", string[][] header = null, string body = null)
+
+        public static void Read()
         {
-            byte[] bodyBytes = null;
-            if (body != null)
+            TcpClient s = new TcpClient();
+            s.Connect("dasz.at", 80);
+            NetworkStream stream = s.GetStream();
+            Write(stream);
+            StreamReader sr = new StreamReader(stream);
+            while (!sr.EndOfStream)
             {
-                bodyBytes = Encoding.UTF8.GetBytes(body);
+                string line = sr.ReadLine();
+                Console.WriteLine(line);
             }
+        }
 
-            var ms = new MemoryStream();
-            var sw = new StreamWriter(ms, Encoding.ASCII);
-
-            sw.WriteLine("{0} {1} HTTP/1.1", method, url);
-            sw.WriteLine("Host: {0}", host);
-            sw.WriteLine("Connection: keep-alive");
-            sw.WriteLine("Accept: text/html,application/xhtml+xml");
-            sw.WriteLine("User-Agent: Unit-Test-Agent/1.0 (The OS)");
-            sw.WriteLine("Accept-Encoding: gzip,deflate,sdch");
-            sw.WriteLine("Accept-Language: de-AT,de;q=0.8,en-US;q=0.6,en;q=0.4");
-            if (bodyBytes != null)
-            {
-                sw.WriteLine(string.Format("Content-Length: {0}", bodyBytes.Length));
-                sw.WriteLine("Content-Type: application/x-www-form-urlencoded");
-            }
-            if (header != null)
-            {
-                foreach (var h in header)
-                {
-                    sw.WriteLine(string.Format("{0}: {1}", h[0], h[1]));
-                }
-            }
+        public static void Write(NetworkStream stream)
+        {
+            if (stream == null) throw new ArgumentNullException("stream");
+            StreamWriter sw = new StreamWriter(stream);
+            sw.WriteLine("GET / HTTP/1.1");
+            sw.WriteLine("host: dasz.at");
+            sw.WriteLine("connection: close");
             sw.WriteLine();
-
-            if (bodyBytes != null)
-            {
-                sw.Flush();
-                ms.Write(bodyBytes, 0, bodyBytes.Length);
-            }
-
             sw.Flush();
+        }
 
-            ms.Seek(0, SeekOrigin.Begin);
-            return ms;
+        private static void Listen()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, 8081);
+            listener.Start();
+            while (true)
+            {
+                Socket s = listener.AcceptSocket();
+                NetworkStream stream = new NetworkStream(s);
+                StreamReader sr = new StreamReader(stream);
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                    if (string.IsNullOrEmpty(line)) break;
+                }
+
+                StreamWriter sw = new StreamWriter(stream);
+                var body = "<html><body><h1>Hello World!</h1><p>a Text</p></body></html>";
+                sw.WriteLine("HTTP/1.1 200 OK");
+                sw.WriteLine("connection: close");
+                sw.WriteLine("content-type: text/html");
+                sw.WriteLine("content-length: " + body.Length);
+                sw.WriteLine();
+                sw.Write(body);
+                sw.Flush();
+                s.Close();
+            }
         }
     }
 }
