4b30f07e461866eab6f20218496ef9d832d18b8b
daniel <if18b030@fh-technikum.at>
2019-12-17 10:38:31 +0100

    plugin manager

--- MyWebServer/PluginManager.cs
+++ MyWebServer/PluginManager.cs
@@ -1,24 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows;
 using System.Text;
 using System.Threading.Tasks;
 using BIF.SWE1.Interfaces;
+using System.IO;
+using System.Reflection;
 
 namespace MyWebServer
 {
     class PluginManager : IPluginManager
     {
-        public IEnumerable<IPlugin> Plugins => throw new NotImplementedException();
+        public PluginManager()
+        {
+            Plugins = new List<IPlugin>();
+        }
+        public IEnumerable<IPlugin> Plugins  { get; }
 
         public void Add(IPlugin plugin)
         {
-            throw new NotImplementedException();
+            Plugins.Append(plugin);
         }
 
         public void Add(string plugin)
         {
-            throw new NotImplementedException();
+            //Load the DLLs from the Plugins directory
+            if (Directory.Exists(plugin))
+            {
+                string[] files = Directory.GetFiles(plugin);
+                foreach (string file in files)
+                {
+                    if (file.EndsWith(".dll"))
+                    {
+                        Assembly.LoadFile(Path.GetFullPath(file));
+                    }
+                }
+            }
+
+            Type interfaceType = typeof(IPlugin);
+            //Fetch all types that implement the interface IPlugin and are a class
+            Type[] types = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(a => a.GetTypes())
+                .Where(p => interfaceType.IsAssignableFrom(p) && p.IsClass)
+                .ToArray();
+            foreach (Type type in types)
+            {
+                //Create a new instance of all found types
+                Plugins.Append((IPlugin)Activator.CreateInstance(type));
+            }
         }
 
         public void Clear()
