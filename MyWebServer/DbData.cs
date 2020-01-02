0d78507f9c2b8f8d76a59e59b5571f2034c639ac
Marius Hochwald <marius.hochwald@gmail.com>
2020-01-02 16:00:26 +0100

    removed some unnecesssary lines

--- MyWebServer/DbData.cs
+++ MyWebServer/DbData.cs
@@ -12,13 +12,13 @@ namespace MyWebServer
 
     public class DbData
     {
-        public int temperatureData = 20;
         public int negSet = 1;
         public int warri = -90000;
         public DateTime timebois = DateTime.Now;
 
         public void tempDataInsert()
         {
+            int temperatureData = 20;
             string connstring = "Server=127.0.0.1; Port=5432; User ID=postgres; Password=postgres;Database=postgres;";
             NpgsqlConnection db = new NpgsqlConnection(connstring);
             db.Open();
@@ -40,24 +40,36 @@ namespace MyWebServer
         }
         public void genTemData()
         {
-            DbData temper = new DbData();
+            int tempOne, tempTwo;
             string connstring = "Server=127.0.0.1; Port=5432; User ID=postgres; Password=postgres;Database=postgres;";
             NpgsqlConnection db = new NpgsqlConnection(connstring);
             db.Open();
+            NpgsqlCommand cmddata = new NpgsqlCommand("select temp from test ORDER by temp_id Desc Limit 2", db);
+            NpgsqlDataReader readerdata = cmddata.ExecuteReader();
+            readerdata.Read();
+            tempOne = readerdata.GetInt32(0);
+            readerdata.Read();
+            tempTwo = readerdata.GetInt32(0);
+            if(tempOne < tempTwo)
+            {
+                negSet = negSet * -1;
+            }
+            db.Close();
+            db.Open();
             while (true)
             {
+                tempOne += negSet;
+                if (tempOne == 30 || tempOne == -30)
+                {
+                    negSet = negSet * -1;
+                }
                 NpgsqlCommand cmd = new NpgsqlCommand("insert into test(temp, date) values (@p, @q)", db);
-                cmd.Parameters.AddWithValue("p", temperatureData);
+                cmd.Parameters.AddWithValue("p", tempOne);
                 DateTime todayIsInYourHand = DateTime.Now;
                 cmd.Parameters.AddWithValue("q", todayIsInYourHand);
                 // need research cmd.Prepare();
                 cmd.ExecuteNonQuery();
-                temperatureData += negSet;
-                if (temperatureData == 30 || temperatureData == -30)
-                {
-                    negSet = negSet * -1;
-                }
-                Thread.Sleep(1000);
+                Thread.Sleep(60000);
                 /*temperatureData += negSet;
                 NpgsqlCommand cmd = new NpgsqlCommand("Select * from test WHERE temp_id = 1", db);
                 NpgsqlDataReader reader = cmd.ExecuteReader();
